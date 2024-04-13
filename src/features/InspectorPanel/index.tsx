095ef69b65f39bee23380a0c40ed1d53ba6200c3
Samson Ayeni <nazaritee@pm.me>
2024-04-12 23:47:26 -0500

    feat: added a linkage of selector and inspect panel

--- src/features/InspectorPanel/index.tsx
+++ src/features/InspectorPanel/index.tsx
@@ -5,32 +5,78 @@ import { usePanel } from './usePanel';
 
 const InspectorPanel = () => {
   const { getObject } = usePanel();
-  const { src, name } = getObject();
+  const {
+    src,
+    name,
+    yPos,
+    xPos,
+    width,
+    height,
+    layer,
+    flipX,
+    flipY
+  } = getObject();
 
   return (
     <div id={INSPECTOR.$}>
       <header>
       Inspector Panel
       </header>
-      <div>
-        <label>
-          Name
-          <input id={INSPECTOR.Name} placeholder='None' title="object selected" value={name} disabled/>
-        </label>
-        <div className='object'>
-          <img src={src}/>
-        </div>
-        <div className='attributes'>
-          <label>
-            Flip
-            <input id={INSPECTOR.Name} placeholder='None' title="object selected" value={name} disabled/>
-            <input id={INSPECTOR.Name} placeholder='None' title="object selected" value={name} disabled/>
-          </label>
-          <label>
-            Flip
-            <input id={INSPECTOR.Name} placeholder='None' title="object selected" value={name} disabled/>
-          </label>
-        </div>
+      <div className='attributes'>
+        <input className='name' placeholder='None' title="object name" value={name} disabled/>
+        <table>
+          <tbody>
+            <tr>
+              <td>Flip</td>
+              <td className='content'>
+                <div className='tight-group'>
+                  <label title='x-axis'>
+                    X
+                    <input type='checkbox' checked={flipX} onClick={() => console.log('Click me')}/>
+                  </label>
+                  <label title='y-axis'>
+                    Y
+                    <input type='checkbox' checked={flipY}/>
+                  </label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Position</td>
+              <td className='content'>
+                <div className='wide-group values'>
+                  <span>
+                    x:<input value={xPos} disabled/>
+                  </span>
+                  <span>
+                    y:<input value={yPos} disabled/>
+                  </span>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Size</td>
+              <td className='content'>
+                <div className='wide-group values'>
+                  <span>
+                    w:<input value={width} disabled/>
+                  </span>
+                  <span>
+                    h:<input value={height} disabled/>
+                  </span>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Layer</td>
+              <td className='content'>
+                <div className='values'>
+                  <input value={layer} disabled/>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/features/InspectorPanel/usePanel.tsx
+++ src/features/InspectorPanel/usePanel.tsx
@@ -2,13 +2,30 @@ import { selection } from '../Scene';
 
 const usePanel = () => {
   function getObject() {
+    if (selection.value) {
+      const { dx, dy, w, h, l, name, image, flipXY } = selection.value;
+      return {
+        name,
+        src: image,
+        width: w,
+        height: h,
+        xPos: dx,
+        yPos: dy,
+        layer: l,
+        flipX: flipXY[0],
+        flipY: flipXY[1],
+      };
+    }
     return {
-      name: selection.value
-        ? selection.value.name
-        : '',
-      src: selection.value
-        ? selection.value.image
-        : ''
+      name: '',
+      src: '',
+      width: 0,
+      height: 0,
+      xPos: 0,
+      yPos: 0,
+      layer: 0,
+      flipX: false,
+      flipY: false
     };
   }
 
